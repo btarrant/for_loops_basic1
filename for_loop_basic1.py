07cddcc5ec1049f2fd6efd24c05268574a3decf3
Brandon Tarrant <brandontarrant04@gmail.com>
2022-01-05 10:52:13 -0500

    Add odd integers from 0 to 500,000.

--- for_loop_basic1.py
+++ for_loop_basic1.py
@@ -1,16 +1,23 @@
 #Basics
-for x in range (0, 151):
-    print(x)
+#for x in range (0, 151):
+#    print(x)
 
 #Multiples of 5
-for x in range (5, 1005, 5):
-    print(x)
+#for x in range (5, 1005, 5):
+#    print(x)
 
 #Counting, the Dojo Way
-for x in range (1, 101):
-    if x % 10 == 0:
-        print("Coding Dojo")
-    elif x % 5 == 0:
-        print("Coding")
-    else:
-        print(x)
+#for x in range (1, 101):
+#    if x % 10 == 0:
+#        print("Coding Dojo")
+#    elif x % 5 == 0:
+#        print("Coding")
+#    else:
+#        print(x)
+
+#Whoa. That Sucker's Huge
+sum = 0
+for x in range(1, 500001,2):
+    sum += x
+print(sum)
+
